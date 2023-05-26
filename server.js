8785dd665feee1314ab074015debd7afe5a0ff60
oscarlea <oscarlea@yahoo.com>
2023-05-26 19:33:15 -0400

    ...

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("src\data\db.json");
+const router = jsonServer.router("src/data/db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
+const port = 4000;
 server.use(middlewares);
 // Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
+
 server.use(router);
 // Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
+server.listen(port, () => {
+    console.log(`JSON Server is running in ${port}`);
 });
 
 // Export the Server API
